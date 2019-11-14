6f0973d33f7f295c73ce31efe14ed3707a748f87
murruni <murruni@gmail.com>
2019-11-14 20:57:56 -0300

    -

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 const TOKEN_HOST = process.env.TOKEN_HOST || 'localhost';
 const TOKEN_PORT = process.env.TOKEN_PORT || '3001';
